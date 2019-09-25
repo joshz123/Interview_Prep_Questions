f5b1017f6360eecc14eeabd1b7d9e9709900f90f
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-09-25 13:15:32 -0400

    triple step solution created

--- TripleStep.py
+++ TripleStep.py
@@ -1,2 +1,17 @@
 #A child is running up a staircase with N steps. H e can either hop up 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
+values = {0:0,1:1,2:2,3:4}
+def triple_step(n):
+    if(n==1):
+        return 1
+    elif (n==2):
+        return 2
+    elif (n==3):
+        return 4
+    elif (n in values):
+        return values[n]
+    else:
+        values[n]= triple_step(n-1)+triple_step(n-2)+triple_step(n-3)
+        return values[n]
 
+
+print(triple_step(110))
